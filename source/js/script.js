0c41fa1775e1bce726a34886478a75e6daea364e
Aleksandr Bosikov <legionerab3000@yandex.ru>
2019-05-17 01:23:55 +1200

    Настроил сборку и исправил метку на карте

--- source/js/script.js
+++ source/js/script.js
@@ -22,21 +22,17 @@ menuButton.addEventListener("click", function (evt) {
 ymaps.ready(function () {
   document.querySelector('.contacts__map-img').getElementsByClassName.display = 'none';
   var mapCenter = [59.938850, 30.323100];
-  // var iconImageSize = [62, 53];
-  // var iconImageOffset = [-25, -50];
+  var iconCenter = mapCenter;
+  var iconImageSize = [62, 53];
+  var iconImageOffset = [-25, -50];
 
-  // if (document.body.clientWidth >= '768') {
-  //   iconImageSize = [124, 106];
-  //   iconImageOffset = [-60, -100];
-  // }
+  if (document.body.clientWidth >= '768') {
+    iconImageSize = [124, 106];
+    iconImageOffset = [-60, -100];
+  }
 
   if (document.body.clientWidth >= '1300') {
     mapCenter = [59.939000, 30.319555];
-  }
-  else {
-    if (document.body.clientWidth >= '768') {
-      mapCenter = [59.939000, 30.323100];
-    }
   }
 
   var myMap = new ymaps.Map('map', {
@@ -46,22 +42,22 @@ ymaps.ready(function () {
           searchControlProvider: 'yandex#search'
       });
 
-  // var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-  //         hintContent: 'Магазин функционального питания для котов "Cat Energy"',
-  //         balloonContent: 'ул. Большая Конюшенная, д. 19/8, Санкт-Петербург'
-  //     }, {
-  //         // Опции.
-  //         // Необходимо указать данный тип макета.
-  //         iconLayout: 'default#image',
-  //         // Своё изображение иконки метки.
-  //         iconImageHref: '../img/map-pin.png',
-  //         // Размеры метки.
-  //         iconImageSize: iconImageSize,
-  //         // Смещение левого верхнего угла иконки относительно
-  //         // её "ножки" (точки привязки).
-  //         iconImageOffset: iconImageOffset
-  //     });
+  var myPlacemark = new ymaps.Placemark(iconCenter, {
+          hintContent: 'Магазин функционального питания для котов "Cat Energy"',
+          balloonContent: 'ул. Большая Конюшенная, д. 19/8, Санкт-Петербург'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: '../img/map-pin.png',
+          // Размеры метки.
+          iconImageSize: iconImageSize,
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: iconImageOffset
+      });
 
-  // myMap.geoObjects.add(myPlacemark);
+  myMap.geoObjects.add(myPlacemark);
 
 });
